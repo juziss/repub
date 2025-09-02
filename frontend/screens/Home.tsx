281d82508a742966d9adb7d4f744bebb0cecfd3a
Julia Coutinho <julianeta26@gmail.com>
2025-09-02 15:52:26 -0300

    chore: update structure (frontend)

---
+++ frontend/screens/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <p>test</p>
+        </div>
+    )
+}
---
+++ frontend/screens/Login.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MainTabs(){
+    return(
+        <div>
+            <p>test</p>
+        </div>
+    )
+}
